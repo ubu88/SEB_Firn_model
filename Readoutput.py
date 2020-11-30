20f9dcb559ea6371223eeb495aae8196d5d3b77a
ubu88 <42889252+ubu88@users.noreply.github.com>
2020-11-30 16:47:28 +0100

    Update Readoutput.py

--- Readoutput.py
+++ Readoutput.py
@@ -29,12 +29,11 @@ initial_density_profile=simulation_inf['initial_density_profile']
 number_of_layers=simulation_inf['number_of_layers']
 time_period=simulation_inf['time_period']
 
-
-target_hr = 205631
+start_date = datetime.date(1989,1,1)
 target_hr_delta = datetime.timedelta(hours=target_hr)
 
 #Define path to where the output will be placed
-path_to_outputs='python_outputs/'+initial_density_profile+'/'+time_period+'/'+number_of_layers+'/'
+path_to_outputs='python_outputs/'+initial_density_profile+iteration_name+'/'+time_period+'/'+number_of_layers+'/'
 
 
 #Create folder if they dont exstis for the plots and the animations
@@ -50,7 +49,7 @@ f.close()
 
 
 #Adds this name to to output files (Animations and plots):
-name='Kan_U_'+initial_density_profile+'_'+time_period+'_'+number_of_layers
+name='Kan_U_'+initial_density_profile+'_'+time_period+'_'+number_of_layers+iteration_name
 
 
 #Load dataset rho_bin
@@ -110,6 +109,9 @@ init_density = pd.read_csv('Input/Initial state/density/'+initial_density_profil
 density_2012 = pd.read_csv('Input/Initial state/density/RetMIP_density_KAN-U_2012(deleted_NaN).csv', sep=',', header=None)
 
 
+#Load 2013_density profile from data
+density_2013 = pd.read_csv('Input/Initial state/density/DensityProfile_KAN_U_2013_Machguth_et_al.csv', sep=';')
+
 #%%
 
 
@@ -117,7 +119,9 @@ density_2012 = pd.read_csv('Input/Initial state/density/RetMIP_density_KAN-U_201
 ani_rho = fa.density_animation(rho_all, Depth_dataframe)
 ani_rho.save(path_to_outputs+'Animations/Densityprofile_animation'+name+'.mp4')
 
-plt.clf() 
+#ani_rho.event_source.stop()
+#del ani_rho
+plt.clf()
 
 
 ani_T = fa.temperature_animation(T_ice_dataframe, Depth_dataframe)
@@ -125,7 +129,9 @@ ani_T.save(path_to_outputs+'Animations/Temperature_animation'+name+'.mp4')
 
 plt.clf() 
 
-
+#ani_T.event_source.stop()
+#del ani_T
+#plt.close()    
 
 
 #%%
@@ -140,29 +146,32 @@ plt.clf()
 
 
 #Plot initial density_profile from Data
-plt.plot(init_density.iloc[:,1], init_density.iloc[:,0])
+plt.plot(init_density.iloc[:,1], init_density.iloc[:,0], label='initial density profile used as input')
 plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
 plt.ylabel(r'Depth ($m$)')
 plt.gca().invert_yaxis()
+plt.legend()
 plt.savefig(path_to_outputs+'plots/Densityprofile1989_data'+name+'.pdf')
 plt.close()
 
 #%%
 #Plot initial density_profile from Simulation
-plt.plot(rho_all.iloc[0], Depth_dataframe.iloc[0])
+plt.plot(rho_all.iloc[0], Depth_dataframe.iloc[0], label='initial density profile from simulation')
 plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
 plt.ylabel(r'Depth ($m$)')
 plt.gca().invert_yaxis()
+plt.legend()
 plt.savefig(path_to_outputs+'plots/Densityprofile1989_simulation'+name+'.pdf')
 plt.close()
 
 #%%
 #Plot initial density_profile from Simulation and Data sumoultanously
-plt.plot(init_density.iloc[:,1], init_density.iloc[:,0])
-plt.plot(rho_all.iloc[0], Depth_dataframe.iloc[0])
+plt.plot(init_density.iloc[:,1], init_density.iloc[:,0], label='initial density profile used as input')
+plt.plot(rho_all.iloc[0], Depth_dataframe.iloc[0], label='initial density profile from simulation')
 plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
 plt.ylabel(r'Depth ($m$)')
 plt.gca().invert_yaxis()
+plt.legend()
 plt.savefig(path_to_outputs+'plots/Densityprofile1989_simulation_and_data'+name+'.pdf')
 plt.close()
 #%%
@@ -174,57 +183,151 @@ plt.close()
 
 if time_period=='1989-2019':
     #Plot 2012 density_profile from Data
-    plt.plot(density_2012.iloc[:,1], density_2012.iloc[:,0])
+    plt.plot(density_2012.iloc[:,1], density_2012.iloc[:,0], label='2012 Macguth et. al')
     plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
     plt.gca().invert_yaxis()
+    plt.legend()
     plt.ylabel(r'Depth ($m$)')
+
     plt.savefig(path_to_outputs+'plots/Densityprofile2012_data'+name+'.pdf')
     plt.close()
 
 
     #PLot density profile mid-May 2012
-    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr])
+    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr], label='simulation '+str(start_date+ target_hr_delta))
     plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
+    #plt.text(400, 30, str(start_date+ target_hr_delta))
     plt.gca().invert_yaxis()
+    plt.legend()        
     plt.ylabel(r'Depth ($m$)')
     plt.savefig(path_to_outputs+'plots/density_profile_may_2012_simulation'+name+'.pdf')
     plt.close()
 
 
     #Plot initial density_profile from Simulation and Data sumoultanously
-    plt.plot(density_2012[1], density_2012[0])
-    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr])
+    plt.plot(density_2012[1], density_2012[0], label='2012 Macguth et. al')
+    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr], label='simulation '+str(start_date+ target_hr_delta))
     plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
-    plt.gca().invert_yaxis()
     plt.ylabel(r'Depth ($m$)')
-    plt.text(400, 30, str(start_date+ target_hr_delta))
-    plt.ax.legend([ ])
-    plt.savefig(path_to_outputs+'plots/Densityprofile2012_simulation_and_data'+name+'.pdf')
+    #plt.text(400, 30, str(start_date+ target_hr_delta))
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.savefig(path_to_outputs+'plots/Densityprofile2012_simulation_and_data'+name+'_'+str(start_date+ target_hr_delta)+'.pdf')
     plt.close()
 
-    
-        
-    plt.plot(density_2012[1], density_2012[0])
-    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr])
-    plt.plot(diff_dens, density_2012[0])
+    #This block of code generates an array, with differences between the simulation
+    #And the experimental data. The density profiles from simulation and data does not 
+    #have the same dimensions, and the simkulation have density in terms of layers
+    #instead of depth. This Block should take take of this by, creating an-
+    #array, by adding data points with same density as the layers it is in.
+    diff_dens = np.zeros(len(density_2012))
+    print(density_2012)
+    #sqr_diffs=0
+    j=0
+    for i in range(len(diff_dens)):
+        cond=0
+        while cond==0:
+            if density_2012.iloc[i][0] < Depth_dataframe.iloc[target_hr][j]: #and i < Depth_dataframe.iloc[target_hr][j+1]*10
+                diff_dens[i] = density_2012.iloc[:,1][i] - rho_all.iloc[target_hr][j]
+                #sqr_diffs= (density_2012.iloc[:,1][i] - rho_all.iloc[target_hr][j])**2
+                #print(i,j, Depth_dataframe.iloc[target_hr][j], density_2012.iloc[:,1][i] , rho_all.iloc[target_hr][j], density_2012.iloc[:,1][i] - rho_all.iloc[target_hr][j])
+                cond = 1
+            else:
+                j += 1
+
+
+
+
+
+    #print(sqr_diffs)
+
+
+    #diff_dens =density_2012[1]- np.ones(len(density_2012))*300
+
+
+
+    plt.plot(density_2012[1], density_2012[0], label='2012 Macguth et. al')
+    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr], label='simulation '+str(start_date+ target_hr_delta))
+    plt.plot(diff_dens, density_2012[0], label='difference between 2012 and simulation')
     plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
     plt.gca().invert_yaxis()
     plt.ylabel(r'Depth ($m$)')
+    plt.legend()
     plt.savefig(path_to_outputs+'plots/Difference_in_Densityprofile2012_simulation_and_data'+name+'.pdf')
     plt.close()
-    
-    
-    
-    
-    
-    plt.plot(np.gradient(density_2012[1]), density_2012[0])
-    plt.plot(np.gradient(rho_all.iloc[target_hr]), Depth_dataframe.iloc[target_hr])
+
+
+
+
+
+    plt.plot(np.gradient(density_2012[1]), density_2012[0], label='gradient: 2012 Macguth et. al')
+    plt.plot(np.gradient(rho_all.iloc[target_hr]), Depth_dataframe.iloc[target_hr], label=' gradient of: simulation '+str(start_date+ target_hr_delta))
     #plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
     plt.gca().invert_yaxis()
     plt.ylabel(r'Depth ($m$)')
+    plt.legend()
     plt.savefig(path_to_outputs+'plots/Gradients_of_Densityprofile2012_simulation_and_data'+name+'.pdf')
     plt.close()
-    
+
+
+
+    #Same plots but comparing with 2013 core.
+            #Plot 2013 density_profile from Data
+    plt.plot(density_2013.iloc[:,1], density_2013.iloc[:,0], label='2013 Macguth et. al')
+    plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.ylabel(r'Depth ($m$)')
+
+    plt.savefig(path_to_outputs+'plots/Densityprofile2013_data'+name+'.pdf')
+    plt.close()
+
+
+    #PLot density profile mid-May 2013
+    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr], label='simulation '+str(start_date+ target_hr_delta+ datetime.timedelta(hours=6940)))
+    plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
+    plt.text(400, 30, str(start_date+ target_hr_delta+ datetime.timedelta(hours=6940)))
+    plt.gca().invert_yaxis()
+    plt.legend()        
+    plt.ylabel(r'Depth ($m$)')
+    plt.savefig(path_to_outputs+'plots/density_profile_may_2013_simulation'+name+'.pdf')
+    plt.close()
+
+
+    #Plot initial density_profile from Simulation and Data sumoultanously
+    plt.plot(density_2013[1], density_2013[0], label='2013 Macguth et. al')
+    plt.plot(rho_all.iloc[target_hr], Depth_dataframe.iloc[target_hr], label='simulation '+str(start_date+ target_hr_delta))
+    plt.xlabel(r'Density ($\frac{kg}{m^3}$)')
+    plt.ylabel(r'Depth ($m$)')
+    #plt.text(400, 30, str(start_date+ target_hr_delta))
+    plt.gca().invert_yaxis()
+    plt.legend()
+    plt.savefig(path_to_outputs+'plots/Densityprofile2013_simulation_and_data'+name+'_'+str(start_date+ target_hr_delta)+'.pdf')
+    plt.close()
+
+    #This block of code generates an array, with differences between the simulation
+    #And the experimental data. The density profiles from simulation and data does not 
+    #have the same dimensions, and the simkulation have density in terms of layers
+    #instead of depth. This Block should take take of this by, creating an-
+    #array, by adding data points with same density as the layers it is in.
+# =============================================================================
+#         diff_dens = np.zeros(len(density_2013))
+#         print(density_2013)
+#         #sqr_diffs=0
+#         j=0
+#         for i in range(len(diff_dens)):
+#             cond=0
+#             while cond==0:
+#                 if density_2013.iloc[i][0] < Depth_dataframe.iloc[target_hr][j]: #and i < Depth_dataframe.iloc[target_hr][j+1]*10
+#                     diff_dens[i] = density_2013.iloc[:,1][i] - rho_all.iloc[target_hr][j]
+#                     #sqr_diffs= (density_2013.iloc[:,1][i] - rho_all.iloc[target_hr][j])**2
+#                     #print(i,j, Depth_dataframe.iloc[target_hr][j], density_2013.iloc[:,1][i] , rho_all.iloc[target_hr][j], density_2013.iloc[:,1][i] - rho_all.iloc[target_hr][j])
+#                     cond = 1
+#                 else:
+#                     j += 1
+#                     
+# =============================================================================
+
 
 
 
